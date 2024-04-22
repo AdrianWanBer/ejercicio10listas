f6fe02fcbc5bc8413da0b8993d49f4d47a3938b3
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2024-04-22 13:34:42 +0200

    Agregar archivos de proyecto.

---
+++ ejercicio10listas/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ejercicio10listas
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            if (numeros.Contains(2))
+            {
+                Console.WriteLine("Si");
+            }
+
+            //Se utiliza la lista porque es la mas rapida y eficiente.
+        }
+    }
+}
